f097d5bba282ebced9d302c4b57b06201502044d
XxEMARxX <essa.mohammed001@gmail.com>
2025-12-06 06:32:00 +0530

    Update Assignment1 Task1.py

--- Assignment1 Task1.py
+++ Assignment1 Task1.py
@@ -6,3 +6,11 @@ print("Addition=", num1+num2)
 print("Subtraction=", num2-num1)
 print("Mutiplication=", num1*num2)
 print("Division=", num2/num1)
+Enter the First Number 60
+Enter the Second Number 6
+num1= 60
+num2= 6
+Addition= 66
+Subtraction= -54                                                                 ^C
+Mutiplication= 360
+Division= 0.1
